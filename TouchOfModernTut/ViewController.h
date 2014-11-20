ba4b72b1584f7af1f1017b6281574699884c0a90
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2014-11-19 21:53:05 -0800

    Initial Commit

---
+++ TouchOfModernTut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchOfModernTut
+//
+//  Created by Hugo on 11/19/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TouchOfModernTut/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TouchOfModernTut
+//
+//  Created by Hugo on 11/19/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
